2476dac30c6b5790d803f67678024261e3f36e8a
Anuj <anujshrivastava982@gmail.com>
2024-12-12 22:01:31 +0530

    fix: pricing

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -4,11 +4,11 @@ function Pricing() {
   return (
     <div className="w-full flex justify-center mt-20">
       <div className="relative">
-        <div className="absolute inset-0 flex flex-col items-center justify-center px-5 text-center z-10 max-w-[710px] mx-auto transform translate-x-1/4 space-y-5">
-          <div className="text-[40px] font-medium">
+        <div className=" relative bg-[#D9DEEF] md:bg-transparent md:absolute inset-0 flex flex-col items-center justify-center px-5 text-center z-10 max-w-[710px] mx-auto space-y-5 py-9 rounded-[30px] md:rounded-none md:transform md:translate-x-1/4 ">
+          <div className="text-xl md:text-[40px] font-medium md:leading-[50px]">
             We work on a month-to-month basis, try for free with no commitments{" "}
           </div>
-          <div className="text-lg text-gray-600 max-w-[584px]">
+          <div className="text-sm md:text-lg text-gray-600 max-w-[584px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </div>
@@ -17,7 +17,11 @@ function Pricing() {
           </button>
         </div>
 
-        <img src="./pricing.png" alt="Pricing" className="w-full" />
+        <img
+          src="./pricing.png"
+          alt="Pricing"
+          className="w-full hidden md:block"
+        />
       </div>
     </div>
   );
